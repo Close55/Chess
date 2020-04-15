41f7704f87b3317790faa85b039b5389044e5419
Close55 <61093132+Close55@users.noreply.github.com>
2020-04-14 20:17:47 -0400

    Add files via upload

---
+++ Board.cpp
@@ -0,0 +1,24 @@
+#include "Board.h"
+#include <iostream>
+using namespace std;
+
+void Board::printBoard() {
+	for (int i = 0; i < 35; i++) {
+		for (int j = 0; j < 53; j++) {
+				cout << displayBoard[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void Board::board() {
+
+
+
+}
+
+void Board::setBoard() {
+
+
+
+}
